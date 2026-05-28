32920303f2ea49cbcae39b8749ee0d891400572c
Div <dhivya.muthusamy15@gmail.com>
2026-05-28 14:18:24 -0400

    Added unstructured data processing workflow

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
+from app.processor import save_json
 from processor import read_file, process_text
 text=read_file("../input/sample.txt")
 result=process_text(text)
-print(result)
+save_json("../output/output.txt", result)
--- app/processor.py
+++ app/processor.py
@@ -1,9 +1,23 @@
+
+import json
 def read_file(file_path):
     with open(file_path, 'r') as f:
         return f.read()
 
 def process_text(text):
-    return {
-        "summary":text[:50],
-        "category":"Claims"
-    }
+    category="Unknown"
+    if "claim" in text.lower():
+        category="Claim"
+    if "provider" in text.lower():
+        issue_type="Missing Provider Information"
+    else:
+        issue_type="General Issue"
+    processed_data = {
+        "Summary": text[:100],
+        "category": category,
+        "issue_type": issue_type
+  }
+    return processed_data
+def save_json(file_path, data):
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=4)
